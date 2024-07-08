9e7b5caed95f61b16c06a59fed24a3fe27263506
Valterlei Mury Viana <valterlei@mte-thomson.com.br>
2024-07-08 17:41:57 -0300

    Limpeza

--- TechChallengeFIAP.IntegrationTests/IntegrationTestWebApplicationFactory.cs
+++ TechChallengeFIAP.IntegrationTests/IntegrationTestWebApplicationFactory.cs
@@ -1,14 +1,8 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Testing;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace TechChallengeFIAP.IntegrationTests
 {
     public class IntegrationTestWebApplicationFactory : WebApplicationFactory<TechChallengeFIAPAPI>
     {
-        protected override void ConfigureWebHost(IWebHostBuilder builder)
-        {
-            builder.UseEnvironment("Testing");
-            base.ConfigureWebHost(builder);
-        }
     }
 }
