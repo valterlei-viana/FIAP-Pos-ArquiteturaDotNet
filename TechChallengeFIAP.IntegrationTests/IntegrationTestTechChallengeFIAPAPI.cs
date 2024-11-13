4a1c28c58b96a9dbf93597c4b3212228cc6e1e92
Valterlei Mury Viana <valterlei.viana@gmail.com>
2024-11-13 18:54:48 -0300

    Environment Test para testar local ou no Github

--- TechChallengeFIAP.IntegrationTests/IntegrationTestTechChallengeFIAPAPI.cs
+++ TechChallengeFIAP.IntegrationTests/IntegrationTestTechChallengeFIAPAPI.cs
@@ -10,21 +10,6 @@ namespace TechChallengeFIAP.IntegrationTests
 {
     public class IntegrationTestTechChallengeFIAPAPI : WebApplicationFactory<TechChallengeFIAPAPI>
     {
-        protected override TestServer CreateServer(IWebHostBuilder builder)
-        {
-            return base.CreateServer(builder);
-        }
-
-        protected override IHost CreateHost(IHostBuilder builder)
-        {
-            return base.CreateHost(builder);
-        }
-
-        protected override void ConfigureClient(HttpClient client)
-        {
-            base.ConfigureClient(client);
-        }
-
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.UseEnvironment("Test");
